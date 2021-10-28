ea1e7c1c90afee543f20ee1db729a560e4a9428a
DanishElephant <93323198+DanishElephant@users.noreply.github.com>
2021-10-28 13:14:16 +0200

    Create kommandoer.bash

---
+++ kommandoer.bash
@@ -0,0 +1,9 @@
+#De kommandoer Kasper indtastede for at starte robotten i onsdags
+
+#source /opt/ros/kinetic/setup.bash
+roslaunch turtlebot_bringup minimal.launch
+
+#source catkin_ws/devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+source catkin_ws/devel/setup.bash
+rosrun turtlemovement turtlemovement_node
