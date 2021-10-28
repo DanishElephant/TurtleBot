a8aeb6e2200aa518fe27b32b2c92eb0af7a56a0b
DanishElephant <93323198+DanishElephant@users.noreply.github.com>
2021-10-28 13:43:42 +0200

    Update kommandoer.bash

--- kommandoer.bash
+++ kommandoer.bash
@@ -1,6 +1,6 @@
 #De kommandoer Kasper indtastede for at starte robotten i onsdags
 
-#source /opt/ros/kinetic/setup.bash
+source /opt/ros/kinetic/setup.bash
 roslaunch turtlebot_bringup minimal.launch
 
 #source catkin_ws/devel/setup.bash
